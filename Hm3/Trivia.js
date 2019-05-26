0a11b80c2161ea54e003b6e35d2f3bccb1e9f08e
kais abu jabal <kaiss_K_W@hotmail.com>
2019-05-26 22:01:32 +0300

    trivia
    Implementing trivia js + jquery.

---
+++ Hm3/Trivia.js
@@ -0,0 +1,65 @@
+
+		var currentQuestion;
+        var num_Of_Questions=0;
+        var questions;
+        function BackToHome()
+        {
+            localStorage.setItem("ChangeUserName",true);
+            window.location.href = "HomePage.html";
+        }
+		$(document).ready(function(){
+			$.ajax({
+				url: "https://opentdb.com/api.php?amount=3&type=multiple",
+				type: "get",
+				success: function(data){
+					questions = data.results;
+                    if(data.results.length === 0){
+						$("#question").text("no questions!!");
+					} else{
+						currentQuestion = data.results[0];
+						console.log(currentQuestion);
+						$("#question").append("<h1 id='curr'>" + 
+							currentQuestion.question + 
+							"</h1>");
+						$("#question").append("<div id='correct' class='answer correct'>" + 
+							currentQuestion.correct_answer + 
+							"</div>");
+						$.each(currentQuestion.incorrect_answers,
+						function(index, value){
+							$("#question").append("<div class='answer' id='answer"+index+"'>" + 
+							value + 
+							"</div>");
+						});
+					}
+				},
+				error:function(err){
+					console.log(err);
+				}
+			});
+			
+			$(document).on('click', '.answer', function(){
+				if($(this).hasClass('correct'))
+                {
+                    if(num_Of_Questions < 2)
+                    {
+                        num_Of_Questions++;
+                        NextQuestion(num_Of_Questions);
+                    }
+                    else{
+                        window.location.href = "Jokes.hml";
+                    }
+				}
+                else{
+                    alert('incorrect answer');
+                }
+			});
+			function NextQuestion(index)
+            {
+                $("#curr").html(questions[index].question);
+                $("#correct").html(questions[index].correct_answer);
+                $("#answer0").html(questions[index].incorrect_answers[0]);
+                $("#answer1").html(questions[index].incorrect_answers[1]);
+                $("#answer2").html(questions[index].incorrect_answers[2]);
+
+            }
+		});
