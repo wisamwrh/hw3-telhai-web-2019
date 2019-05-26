d3887ba5c91693361de7e8b8a9320ad5aecbedb7
wisamwrh <48412412+wisamwrh@users.noreply.github.com>
2019-05-27 01:20:33 +0300

    Update HomePage.js

--- Hm3/HomePage.js
+++ Hm3/HomePage.js
@@ -4,7 +4,7 @@ function login() {
         localStorage.setItem("userName", input.value);
         localStorage.setItem("ChangeUserName", false);
         console.log(input.value);
-        window.location.href = 'https://google.com';
+        window.location.href = 'Trivia.html';
     }
     else{
         alert("You must enter a username");
